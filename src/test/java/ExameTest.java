bcd9083384db43051ea764507b1f07b6598dd62a
gabsgc <gabriellagarccia@gmail.com>
2022-09-10 11:41:52 -0300

    test: verificar nivel de colesterol HDL, LDL e VLDL

--- src/test/java/ExameTest.java
+++ src/test/java/ExameTest.java
@@ -84,4 +84,127 @@ public class ExameTest {
 
         assertEquals("Elevado", exame.verificarNivelColesterolTotal());
     }
+
+    @Test
+    public void quandoVerificarColesterolHDL_deveLancarExcecao() {
+        try {
+            Exame exame = new Exame();
+            exame.setColesterolHDL(0);
+            exame.verificarNivelColesterolHDL();
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Não foi possível verificar o nível de Colesterol HDL.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void quandoVerificarColesterolHDL_deveRetornarBaixo() {
+        Exame exame = new Exame();
+        exame.setColesterolHDL(39);
+
+        assertEquals("Baixo", exame.verificarNivelColesterolHDL());
+    }
+
+    @Test
+    public void quandoVerificarColesterolHDL_deveRetornarNormal() {
+        Exame exame = new Exame();
+        exame.setColesterolHDL(60);
+
+        assertEquals("Normal", exame.verificarNivelColesterolHDL());
+    }
+
+    @Test
+    public void quandoVerificarColesterolHDL_deveRetornarDesejavel() {
+        Exame exame = new Exame();
+        exame.setColesterolHDL(61);
+
+        assertEquals("Desejável", exame.verificarNivelColesterolHDL());
+    }
+
+    @Test
+    public void quandoVerificarColesterolLDL_deveLancarExcecao() {
+        try {
+            Exame exame = new Exame();
+            exame.setColesterolHDL(0);
+            exame.verificarNivelColesterolLDL();
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Não foi possível verificar o nível de Colesterol LDL.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void quandoVerificarColesterolLDL_deveRetornarOtimo() {
+        Exame exame = new Exame();
+        exame.setColesterolLDL(99);
+
+        assertEquals("Ótimo", exame.verificarNivelColesterolLDL());
+    }
+
+    @Test
+    public void quandoVerificarColesterolLDL_deveRetornarDesejavel() {
+        Exame exame = new Exame();
+        exame.setColesterolLDL(129);
+
+        assertEquals("Desejável", exame.verificarNivelColesterolLDL());
+    }
+
+    @Test
+    public void quandoVerificarColesterolLDL_deveRetornarLimitrofe() {
+        Exame exame = new Exame();
+        exame.setColesterolLDL(159);
+
+        assertEquals("Limítrofe", exame.verificarNivelColesterolLDL());
+    }
+
+    @Test
+    public void quandoVerificarColesterolLDL_deveRetornarAlto() {
+        Exame exame = new Exame();
+        exame.setColesterolLDL(189);
+
+        assertEquals("Alto", exame.verificarNivelColesterolLDL());
+    }
+
+    @Test
+    public void quandoVerificarColesterolLDL_deveRetornarMuitoAlto() {
+        Exame exame = new Exame();
+        exame.setColesterolLDL(190);
+
+        assertEquals("Muito alto", exame.verificarNivelColesterolLDL());
+    }
+
+    @Test
+    public void quandoVerificarColesteroVLDL_deveLancarExcecao() {
+        try {
+            Exame exame = new Exame();
+            exame.setColesterolVLDL(0);
+            exame.verificarNivelColesterolVLDL();
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Não foi possível verificar o nível de Colesterol VLDL.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void quandoVerificarColesterolVLDL_deveRetornarNivelDesejavel() {
+        Exame exame = new Exame();
+        exame.setColesterolVLDL(29);
+
+        assertEquals("Nível desejável", exame.verificarNivelColesterolVLDL());
+    }
+    @Test
+    public void quandoVerificarColesterolVLDL_deveRetornarNivelLimitrofe() {
+        Exame exame = new Exame();
+        exame.setColesterolVLDL(40);
+
+        assertEquals("Nível limítrofe", exame.verificarNivelColesterolVLDL());
+    }
+
+    @Test
+    public void quandoVerificarColesterolVLDL_deveRetornarNivelElevado() {
+        Exame exame = new Exame();
+        exame.setColesterolVLDL(41);
+
+        assertEquals("Nível elevado", exame.verificarNivelColesterolVLDL());
+    }
 }
